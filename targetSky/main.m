18afd8ce68f61b4ebfffe4c89991d0c3dfe19f0a
Samuel Mascarell <Samuel@MacBook-de-Samuel.local>
2013-05-19 21:27:37 +0200

    Initial Commit

---
+++ targetSky/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  targetSky
+//
+//  Created by Samuel Mascarell on 19/05/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
